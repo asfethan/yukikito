299f04f149a0dec18b07ff105f5137570e0562c6
Kyle Reynolds <kyle@canvasfoundation.org>
2010-04-27 18:59:56 -0400

    * Fixed directory of image file for rendertest.
    Signed-off-by: Kyle Reynolds <kyle@canvasfoundation.org>

--- src/rendertest.cpp
+++ src/rendertest.cpp
@@ -24,140 +24,12 @@ SDL_Event event;
 bool quit;
 GLuint texture;
 GLenum error;
-class Timer
-{
-    private:
-    //The clock time when the timer started
-    int startTicks;
-
-    //The ticks stored when the timer was paused
-    int pausedTicks;
-
-    //The timer status
-    bool paused;
-    bool started;
-
-    public:
-    //Initializes variables
-    Timer();
-
-    //The various clock actions
-    void start();
-    void stop();
-    void pause();
-    void unpause();
-
-    //Gets the timer's time
-    int get_ticks();
-
-    //Checks the status of the timer
-    bool is_started();
-    bool is_paused();
-};
-
-Timer::Timer()
-{
-    //Initialize the variables
-    startTicks = 0;
-    pausedTicks = 0;
-    paused = false;
-    started = false;
-}
-
-void Timer::start()
-{
-    //Start the timer
-    started = true;
-
-    //Unpause the timer
-    paused = false;
-
-    //Get the current clock time
-    startTicks = SDL_GetTicks();
-}
-
-void Timer::stop()
-{
-    //Stop the timer
-    started = false;
-
-    //Unpause the timer
-    paused = false;
-}
-
-void Timer::pause()
-{
-    //If the timer is running and isn't already paused
-    if( ( started == true ) && ( paused == false ) )
-    {
-        //Pause the timer
-        paused = true;
-
-        //Calculate the paused ticks
-        pausedTicks = SDL_GetTicks() - startTicks;
-    }
-}
-
-void Timer::unpause()
-{
-    //If the timer is paused
-    if( paused == true )
-    {
-        //Unpause the timer
-        paused = false;
-
-        //Reset the starting ticks
-        startTicks = SDL_GetTicks() - pausedTicks;
-
-        //Reset the paused ticks
-        pausedTicks = 0;
-    }
-}
-
-int Timer::get_ticks()
-{
-    //If the timer is running
-    if( started == true )
-    {
-        //If the timer is paused
-        if( paused == true )
-        {
-            //Return the number of ticks when the timer was paused
-            return pausedTicks;
-        }
-        else
-        {
-            //Return the current time minus the start time
-            return SDL_GetTicks() - startTicks;
-        }
-    }
-
-    //If the timer isn't running
-    return 0;
-}
-
-bool Timer::is_started()
-{
-    return started;
-}
-
-bool Timer::is_paused()
-{
-    return paused;
-}
-
-
-
-
-
 
 
 int main(int argc, char *argv[]) {
 	g_type_init();
 	rsvg_init();
-	
-	Timer fps;
-	Timer update;
+
 	int frame = 0;
 
 	SDL_Surface *screen = SDL_SetVideoMode( SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_OPENGL );
@@ -197,14 +69,13 @@ int main(int argc, char *argv[]) {
 	Flower = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, FlowerWidth, FlowerHeight);
 	context = cairo_create(Flower);
 	
-	const gchar* Filename = "/game/yuki/resources/sprite/lavender/lavender.svg";
+	const gchar* Filename = "game/yuki/resources/sprite/lavender/lavender.svg";
 	RsvgHandle* SvgData = rsvg_handle_new_from_file(Filename, NULL);
 	
-	cairo_scale(context,50,50);
-	
+	//cairo_scale(context,50,50);
+	cairo_scale(context,.50,.50);
 	rsvg_handle_render_cairo(SvgData, context	);
 	unsigned char *buffer = cairo_image_surface_get_data(Flower);
-	//cairo_surface_write_to_png(Flower,"flower3.png");
 
     //Make a texture
 	glGenTextures(1, &texture);
@@ -232,64 +103,57 @@ int main(int argc, char *argv[]) {
 	buffer); 
 		error = glGetError();
 	std::cout << gluErrorString(error) << " For the teximage2d \n";
-//done
-
-update.start();
-fps.start();
+	//done
+	
 	while (quit==false) {
 		while(SDL_PollEvent(&event)) {
 			if(event.type == SDL_QUIT) { 
-			quit = true; 
+				quit = true; 
 			}
-			}
+		}
 
-			//gotta figure out why I gotta set matrix modes and such
-                glMatrixMode(GL_PROJECTION);
-                glLoadIdentity();
-                gluPerspective(50.0, 1.0, 1.0, 400.0);
-                glMatrixMode(GL_MODELVIEW);
-                glLoadIdentity();
-                gluLookAt(0.0, 0.0, zoom,
-                          0.0, 0.0, 0.0,
-                          0.0, 1.0, 0.0);
-			error = glGetError();
-	std::cout << gluErrorString(error) << " for the glulookat \n";
+		//gotta figure out why I gotta set matrix modes and such
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		gluPerspective(50.0, 1.0, 1.0, 400.0);
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+		gluLookAt(0.0, 0.0, zoom,
+				  0.0, 0.0, 0.0,
+				  0.0, 1.0, 0.0);
+		error = glGetError();
+		std::cout << gluErrorString(error) << " for the glulookat \n";
 		zoom+=.1;
 		std::cout << zoom;
 
-    glBindTexture (GL_TEXTURE_2D, texture);
-	error = glGetError();
-	std::cout << gluErrorString(error) << "lolololol for the texturebind\n";
-    glBegin (GL_QUADS);
-
-	glTexCoord2f (0.0, 0.0);
-	glVertex3f (0.0, 0.0, 0.0);
+		glBindTexture (GL_TEXTURE_2D, texture);
+		error = glGetError();
+		std::cout << gluErrorString(error) << "lolololol for the texturebind\n";
+		glBegin (GL_QUADS);
 	
-	glTexCoord2f (1.0, 0.0);
-	glVertex3f (40.0, 0.0, 0.0);
+		glTexCoord2f (0.0, 0.0);
+		glVertex3f (0.0, 0.0, 0.0);
+		
+		glTexCoord2f (1.0, 0.0);
+		glVertex3f (40.0, 0.0, 0.0);
+		
+		glTexCoord2f (1.0, 1.0);
+		glVertex3f (40.0, 40.0, 0.0);
+		
+		glTexCoord2f (0.0, 1.0);
+		glVertex3f (0.0, 40.0, 0.0);
 	
-	glTexCoord2f (1.0, 1.0);
-	glVertex3f (40.0, 40.0, 0.0);
+		glEnd ();
+		error = glGetError();
+		std::cout << gluErrorString(error) << " for the QUAD\n  ";
 	
-	glTexCoord2f (0.0, 1.0);
-	glVertex3f (0.0, 40.0, 0.0);
-
-    glEnd ();
-	error = glGetError();
-	std::cout << gluErrorString(error) << " for the QUAD\n  ";
-
-
+	
 		SDL_GL_SwapBuffers();
 		glClear( GL_COLOR_BUFFER_BIT );	
 		glClearColor(0.8f, 0.8f, 1.0f, 1.0f);	
 		glGetError();
-frame++;
-//quit = true;
-if (update.get_ticks() > 1000) {
-	std::cout << "FPS: " << (frame/fps.get_ticks()/1000) << "\n";
-	update.start();
-}
-		}
-	
+		frame++;
+
+	}	
 }
 		
